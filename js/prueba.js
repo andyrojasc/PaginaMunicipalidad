8886e8518ceeccb06113679e71def2f0a2ffcce5
AndyRojas <andyrojasucr@gmail.com>
2018-10-09 23:33:04 -0600

    Commit contacts 1
    Commit contacts 1

--- js/prueba.js
+++ js/prueba.js
@@ -1,39 +1,36 @@
     // Funcion de cambiar el color del nav al hacer scroll hacia abajo
-    $(document).ready(function() {
-      $(window).scroll(function(){
-        if (screen.width > 768) {
-          console.log('entró pc');
-          if ($(this).scrollTop() > 20) {
-            $('nav').addClass('fondo');
-            
-          } else {
-            $('nav').removeClass('fondo');
+    /*  $(document).ready(function() {
+        $(window).scroll(function(){
+          if (screen.width > 768) {
+            console.log('entró pc');
+            if ($(this).scrollTop() > 20) {
+              $('nav').addClass('fondo');
+              
+            } else {
+              $('nav').removeClass('fondo');
+            }
           }
-        }
-        else{
-          console.log("entró Responsive");
-          if ($(this).scrollTop() > 20) {
-            $('nav').addClass('fondo');
-          } 
-          else {
-            $('nav').removeClass('fondo');
+          else{
+            console.log("entró Responsive");
+            if ($(this).scrollTop() > 20) {
+              $('nav').addClass('fondo');
+            } 
+            else {
+              $('nav').removeClass('fondo');
+            }
           }
-        }
-      });
-    });
+        });
+      });*/
 
-function quitarIMG()
-{
-	document.getElementById("img").classList.remove('letras');
-    document.getElementById("img").classList.remove('letrasCambio');
-}
+    function quitarIMG() {
+        document.getElementById("img").classList.remove('letras');
+        document.getElementById("img").classList.remove('letrasCambio');
+    }
 
-function efectobluir()
-{
-  document.getElementById("contenido-pagina").classList.toggle('efectoBluir');
-}
+    function efectobluir() {
+        document.getElementById("contenido-pagina").classList.toggle('efectoBluir');
+    }
 
-$('.carouselExampleIndicators').carousel({
-  interval: 300000000000000000000
-})
-
+    $('.carouselExampleIndicators').carousel({
+        interval: 300000000000000000000
+    })
